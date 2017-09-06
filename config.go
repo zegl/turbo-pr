312a739855e73278ab08ea2a2925e093e2788439
Gustav Westling <hello@gustav.tv>
2017-09-06 20:40:26 +0200

    fea(turbo-pr) added testing and more checks

---
+++ config.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	PullRequest struct {
+		MaxAllowedCommits *int `yaml:"maxAllowedCommits"`
+	} `yaml:"pullRequest"`
+
+	Commit struct {
+		MaxSubjectLength      *int     `yaml:"maxSubjectLength"`
+		MinSubjectLength      *int     `yaml:"minSubjectLength"`
+		SubjectMustMatchRegex []string `yaml:"subjectMustMatchRegex"`
+
+		MaxBodyMessageLength *int `yaml:"maxBodyMessageLength"`
+	} `yaml:"commit"`
+}
+
+func getConfig(configFile string) *Config {
+	configData, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var conf Config
+
+	err = yaml.Unmarshal(configData, &conf)
+	if err != nil {
+		panic(err)
+	}
+
+	return &conf
+}
